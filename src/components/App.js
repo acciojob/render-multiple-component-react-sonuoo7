93cf465ae26891906854616f203fdb47c1e75028
Sonu Pant007 <60938647+sonuoo7@users.noreply.github.com>
2023-02-26 20:29:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <h2 data-ns-test="project-name">Recipe Search</h2>
+      <p data-ns-test="project-description">A web application that allows users to search for recipes based on keywords or ingredients. Users can also save their favorite recipes and create a shopping list based on the ingredients needed.</p>
+    </div>
   )
 }
 
